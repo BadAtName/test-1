26c88607969984ed7afe796fce1016d6ebd58778
BadAtName <gevinve70@gmail.com>
2025-09-14 21:02:29 +0700

    navbar left need fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "WE THE BEST DEVELOPER",
   description: "67",
@@ -24,11 +14,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+<body>
+      <div className="wrapper">
+        <div><a className="logo m-1 " href="/">SEDULURGEN</a></div>
+          <div className="nig">
+            <ul className="list-none m-0 p-0 overflow-hidden float-right">
+                <li className="float-right"><a className="shit" href="/value">Value</a></li>
+                <li className="float-right"><a className="shit" href="/artikel">Article</a></li>
+                <li className="float-right"><a className="shit" href="/programs">Programs</a></li>    
+                <li className="float-right"><a className="shit" href="/about">About Us</a></li>
+           </ul>
+        </div>
+      </div>
+
+    <div className="h-50px"></div>
         {children}
       </body>
-    </html>
+  </html>
   );
 }
