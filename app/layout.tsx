0b441560cc7455f46aa9567d216f76098ed322f2
BadAtName <gevinve70@gmail.com>
2025-09-15 10:09:09 +0700

    NAVBAR AND COMUNITY BUTTON DONE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "WE THE BEST DEVELOPER",
@@ -15,16 +15,30 @@ export default function RootLayout({
   return (
     <html lang="en">
 <body>
-      <div className="wrapper">
-        <a className="logo" href="/">SEDULURGEN</a>
+    <div className="wrapper">
+      <a className="logo" href="/">SEDULURGEN</a>
         <div className="nig">
             <a className="shit" href="/value">Value</a>
             <a className="shit" href="/artikel">Article</a>
             <a className="shit" href="/programs">Programs</a>  
             <a className="shit" href="/about">About Us</a>
-            </div>
-      </div>
+        </div>
+    </div>
 
+    <div className="comunity_button">
+                <a href="https://chat.whatsapp.com/JQqLhR7Wx8e5cABlcMW8He">
+                <ul className="com_ul">
+                    <Image 
+                      src="/waigger.svg"
+                      width={40}
+                      height={40}
+                      alt="whatsapp logo"
+                      className="px-2 py-2"
+                    />
+                    <h3 className="font-bold">JOIN KOMUNITAS</h3>
+                </ul>
+                </a>
+            </div>
     <div className="h-50px"></div>
         {children}
       </body>
